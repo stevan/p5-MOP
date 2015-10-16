cdbea5c661d12ac952f354012b3c6a655b46e684
Stevan Little <stevan.little@booking.com>
2015-10-16 22:02:53 +0200

    add a root mop module

---
+++ lib/mop.pm
@@ -0,0 +1,25 @@
+package mop;
+
+use v5.10;
+
+use strict;
+use warnings;
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+use mop::object;
+
+use mop::module;
+
+use mop::role;
+use mop::class;
+
+use mop::attribute;
+use mop::method;
+
+use mop::util ();
+
+1;
+
+__END__
