f6879092bed7f55ee54585304a7b966ce678f54d
Stevan Little <stevan.little@booking.com>
2017-01-12 14:02:33 +0100

    just adding some docs, WIP, always

--- lib/MOP.pm
+++ lib/MOP.pm
@@ -37,20 +37,51 @@ __END__
 
 =head1 DESCRIPTION
 
-This module implements a Meta Object Protocol for Perl 5. 
+This module implements a Meta Object Protocol for Perl 5 with minimal 
+overhead and no non-core dependencies (eventually). 
+
+A Meta Object Protocol, or MOP, is an API to the various parts of the 
+an object system. 
 
 =head1 CONCEPTS
 
+There are only a few key concepts in the MOP, which are described below.
+
 =head2 L<MOP::Role>
+
+A role is simply a package which I<may> have methods, I<may> have slot 
+defintions, and I<may> consume other roles. 
 
 =head2 L<MOP::Class>
 
+A class I<does> all the things a role does, with the addition of 
+inheritance and instance construction. 
+
 =head2 L<MOP::Slot>
 
+A slot is best thought of as representing a single entry in the 
+package scoped C<%HAS> variable. This is basically just building upon the 
+conceptual model laid out by L<UNIVERSAL::Object>.
+
 =head2 L<MOP::Method>
+
+A method is simply a wrapper around a reference to a CODE slot inside 
+a given package. 
 
 =head1 SEE ALSO 
 
 =head2 L<UNIVERSAL::Object>
+
+This module uses the L<UNIVERSAL::Object> module as the chosen instance 
+construction protocol, but also in it's introspection assumes that a 
+class uses the conventions of L<UNIVERSAL::Object> specifically with 
+regards to slot storage.
+
+=head2 L<Class::MOP>
+
+Almost 10 years ago I wrote L<Class::MOP>, whose purpose was also to 
+bring a MOP to Perl 5. While these modules may have had the same goal, 
+they have different requirements and priorities and so shouldn't be 
+compared directly. 
 
 =cut
