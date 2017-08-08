47438736b41348636741a146fa7963108c29aa2b
Stevan Little <stevan.little@gmail.com>
2017-08-08 14:24:51 +0200

    release engineering for 0.08

--- lib/MOP/Slot.pm
+++ lib/MOP/Slot.pm
@@ -10,7 +10,7 @@ use UNIVERSAL::Object::Immutable;
 
 use MOP::Internal::Util;
 
-our $VERSION   = '0.07';
+our $VERSION   = '0.08';
 our $AUTHORITY = 'cpan:STEVAN';
 
 our @ISA; BEGIN { @ISA = 'UNIVERSAL::Object::Immutable' }
