cde3a684d9901ab28358c188943c6fccef9c63b2
Stevan Little <stevan.little@booking.com>
2017-10-12 14:40:15 +0200

    add another MOP::Util function

--- lib/MOP/Util.pm
+++ lib/MOP/Util.pm
@@ -4,12 +4,24 @@ package MOP::Util;
 use strict;
 use warnings;
 
+use MOP::Role;
 use MOP::Internal::Util ();
 
 our $VERSION   = '0.09';
 our $AUTHORITY = 'cpan:STEVAN';
 
 sub APPLY_ROLES { MOP::Internal::Util::APPLY_ROLES( @_ ) }
+
+sub INHERIT_SLOTS {
+    my ($meta) = @_;
+    foreach my $super ( map { MOP::Role->new( name => $_ ) } @{ $meta->mro } ) {
+        foreach my $slot ( $super->slots ) {
+            $meta->alias_slot( $slot->name, $slot->initializer )
+                unless $meta->has_slot( $slot->name )
+                    || $meta->has_slot_alias( $slot->name );
+        }
+    }
+}
 
 1;
 
