fb83edda95ed5b11ee522d5ef22b7ddf0fb2911e
Stevan Little <stevan.little@gmail.com>
2017-10-24 00:14:46 +0200

    add support for METACLASS method in a package to provide a custom class to create when MOP::Util::get_meta is called.

--- lib/MOP/Util.pm
+++ lib/MOP/Util.pm
@@ -14,6 +14,9 @@ our $AUTHORITY = 'cpan:STEVAN';
 sub get_meta {
     my ($package) = @_;
 
+    return $package->METACLASS->new( $package )
+        if $package->can('METACLASS');
+
     my $meta = MOP::Role->new( $package );
     my $isa  = MOP::Internal::Util::GET_GLOB_SLOT( $meta->stash, 'ISA', 'ARRAY' );
 
@@ -83,15 +86,24 @@ This is a public API of MOP related utility functions.
 
 =item C<get_meta( $package )>
 
-This will guess the most sensible type of meta object to represent
-the C<$package> supplied. The test is simple, if there is anything
-in the C<@ISA> array within C<$package>, then it is clearly a class
-and therefore returns a L<MOP::Class> instance. However, if there
-is nothing in C<@ISA> we conservatively estimate that this is a role
-and return a L<MOP::Role> instance.
+First this will check to see if C<$package> has a C<METACLASS>
+method, and if so, will use it to construct the metaclass and
+return it to you.
+
+If no C<METACLASS> method is found, this function will next attempt
+to guess the most sensible type of meta object for the C<$package>
+supplied.
+
+The test is simple, if there is anything in the C<@ISA> array inside
+C<$package>, then it is clearly a class and then this function returns
+a L<MOP::Class> instance. However, if there is nothing in C<@ISA> we
+conservatively estimate that this is a role and then return a
+L<MOP::Role> instance.
 
 In pretty much all cases that matter, a role and a class are entirely
-interchangable.
+interchangable. The only real difference is that a class has methods
+in the MOP for manipulating inheritance relationships (C<@ISA>)and
+roles do not.
 
 =item C<compose_roles( $meta )>
 
